257407df772b6d918ce6bfb88d6829121456b480
JurnGG <jurn.gielis@gmail.com>
2026-03-23 00:43:11 +0100

    add city validation with the city list

--- responsive_formulier/src/validators.ts
+++ responsive_formulier/src/validators.ts
@@ -1,3 +1,5 @@
+import { steden } from './steden';
+
 export function validateName(name: string): boolean {
   const nameParts = name.trim().split(' ');
   return nameParts.every((part) => part.length >= 2);
@@ -8,8 +10,8 @@ export function validateAddress(address: string): boolean {
 }
 
 export function validateCity(city: string): boolean {
-  // Not implemented no steden.json
-  return true;
+  const stedenlist = new Set(steden);
+  return stedenlist.has(city.toLowerCase());
 }
 
 export function validateZipcode(zipcode: string) {
